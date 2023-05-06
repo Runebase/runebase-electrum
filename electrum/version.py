58b4cb8796691c1d4a590b5a7f24faa536fa6b78
bobpepers <bob.pepers@gmail.com>
2023-05-06 16:04:06 +0200

    bump version

--- electrum/version.py
+++ electrum/version.py
@@ -1,5 +1,5 @@
-ELECTRUM_VERSION = '4.0.17'     # version of the client package
-APK_VERSION = '4.0.0.17'        # read by buildozer.spec
+ELECTRUM_VERSION = '4.0.18'     # version of the client package
+APK_VERSION = '4.0.0.18'        # read by buildozer.spec
 
 PROTOCOL_VERSION = '1.5'     # protocol version requested
 
